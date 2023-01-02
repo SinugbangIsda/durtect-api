4cdbf9c8f212c82d704bf7b95b2c0c46149fe827
SinugbangIsda <jamesweed420x@gmail.com>
2023-01-02 23:29:49 +0800

    init

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+import torch
+import os 
+model = torch.hub.load('ML/yolov5-master', 'custom', path = 'ML/weights/best.pt', source = "local")
+
+app = Flask(__name__)
+
+@app.route('/api/v1/detect', methods = ['POST'])
+def detect():
+    image = request.files['image']
+    file_name = image.filename
+    image.save(file_name)
+    results = model(file_name)                                                                                                                                                                                                                                                                                                    
+    results.save()
+    gege = results.pandas().xyxy[0].to_json()
+    os.remove(file_name)
+    print(gege)
+    return gege
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 5000, debug = True)
