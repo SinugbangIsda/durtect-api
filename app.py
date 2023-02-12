4850b5254f397b3a89527d3981cb108caa0a060e
SinugbangIsda <marcuoperario@gmail.com>
2023-02-12 22:01:38 +0800

    small changes

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 import torch
 import os 
-model = torch.hub.load('ML/yolov5-master', 'custom', path = 'ML/weights/jesus.pt', source = "local")
+import io
+from PIL import Image
+import shutil
 
 app = Flask(__name__)
 
@@ -10,12 +12,15 @@ def detect():
     image = request.files['image']
     file_name = image.filename
     image.save(file_name)
-    results = model(file_name)                                                                                                                                                                                                                                                                                                    
-    results.save()
-    gege = results.pandas().xyxy[0].to_json()
+    detect = model(file_name)                                                                                                                                                                                                                                                                                                    
+    # detect.save()
+    # generated_image_directory = "runs/detect/exp"
+    data = detect.pandas().xyxy[0].to_json()
     os.remove(file_name)
-    print(gege)
-    return gege
+    # shutil.rmtree("runs")
+    print(data)
+    return data
 
 if __name__ == "__main__":
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path = 'ML/weights/jesus.pt')
     app.run(host = "0.0.0.0", port = 5000, debug = True) 
