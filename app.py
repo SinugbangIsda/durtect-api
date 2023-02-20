92046aef302b9b1f80a19db7208e0c6e690994a4
SinugbangIsda <marcuoperario@gmail.com>
2023-02-20 22:52:34 +0800

    sorted data, added empty values and error handling, and added delete routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 import torch
 import os 
 import shutil
@@ -33,55 +33,71 @@ firebase_config = {
 @app.route("/d", methods = ["POST"])
 def detect():
     image = request.files["image"]
-    uid = request.form.get("uid")
+    timestamp = request.form.get("timestamp")
+    user_id = request.form.get("user_id")
     file_name = image.filename
     image.save(file_name)
-    detect = model(file_name)                                                                                                                                                                                                                                                                                                    
-    detect.save()
+    detect = model(file_name)
     results = detect.pandas().xyxy[0].to_json()
     os.remove(file_name)
     results_json = json.loads(results)
+    results_json["timestamp"] = timestamp
 
     if results_json["class"] == {}:
-        results_json["status"] = "NULL"
+        results_json["status"] = "null"
+        results_json["image_uri"] = "null"
+        results_json = { "something": results_json }
     else:
+        detect.save()
+        file_name = file_name.replace(".jpeg", ".jpg")
         local_path = "runs/detect/exp/" + file_name
         cloud_path = "images/" + file_name
         storage.child(cloud_path).put(local_path)
         image_uri = storage.child(cloud_path).get_url(None)
         results_json["image_uri"] = image_uri
         results_json["status"] = "OK"
-        results_json["timestamp"] = 2
-        results_json["uid"] = uid
-        db.child("detections").push(results_json)
-        
-    shutil.rmtree("runs")
-    result = json.dumps(results_json)
-    return result    
+        entry = db.child("detections").child(user_id).push(results_json)
+        data_id = entry["name"]
+        results_json = { data_id: results_json}
+        shutil.rmtree("runs")
+    final_response = json.dumps(results_json)
+    print(final_response)
+    return final_response    
 
-@app.route("/rh", methods = ["GET"])
-def get_recent_history():
-    data = db.child("detections").get()
-    return data.val()
+@app.route("/rh", methods = ["POST"])
+def get_recent_logs():
+    user_id = request.form.get("user_id")
+    data = db.child("detections").child(user_id).order_by_child("timestamp").limit_to_first(5).get()
+    if data.val() == None:
+        response = { "message": "empty"}
+        return response
+    else:
+        return data.val()
 
-@app.route("/h/<detection_id>", methods = ["GET"])
-def get_result():
-    return
+@app.route("/ah", methods = ["POST"])
+def get_all_logs():
+    user_id = request.form.get("user_id")
+    data = db.child("detections").child(user_id).order_by_child("timestamp").get()
+    if data.val() == None:
+        response = { "message": "empty"}
+        return response
+    else:
+        return data.val()
 
-@app.route("/r", methods = ["POST"])
-def register_user():
-    email = request.form.get("email")
-    password = request.form.get("password")
-    print(email, password)
-    auth.create_user_with_email_and_password(email, password)
-    return "success"
+@app.route("/dl", methods = ["POST"])
+def delete_log():
+    user_id = request.form.get("user_id")
+    entry_id = request.form.get("entry_id")
+    db.child("detections").child(user_id).child(entry_id).remove()
+    response = { "message": "deleted"}
+    return response
 
-@app.route("/l", methods = ["POST"])
-def login_user():
-    email = request.form.get("email")
-    password = request.form.get("password")
-    auth.sign_in_with_email_and_password(email, password)
-    return "success"
+@app.route("/dal", methods = ["POST"])
+def delete_all_logs():
+    user_id = request.form.get("user_id")
+    db.child("detections").child(user_id).remove()
+    response = { "message": "deleted"}
+    return response
 
 if __name__ == "__main__":
     model = torch.hub.load("ultralytics/yolov5", "custom", path = "ML/weights/jesus.pt")
