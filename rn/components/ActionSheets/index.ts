e8507c79f1c8993ba95d064fb74a659197475bfc
Marcu Operario <marcuoperario@gmail.com>
2023-03-07 16:59:33 +0800

    Delete rn directory

--- rn/components/ActionSheets/index.ts
+++
@@ -1,6 +0,0 @@
-import { registerSheet } from 'react-native-actions-sheet';
-import DeleteResult from './DeleteResult';
-
-
-registerSheet('delete-result', DeleteResult);
-export {};
--- rn/utils/getDateFromTimestamp.ts
+++
@@ -1,5 +0,0 @@
-import moment, { unix } from "moment"
-
-export const getDateFromTimestamp = (date: any) => {
-    return moment(unix(date.toString())).format("L");
-}
